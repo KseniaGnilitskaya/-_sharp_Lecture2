78652dba4fbf995cdbab6a933197fed838539975
Ksenia Gnilitskaya <ksuha_87@mail.ru>
2022-06-16 20:06:07 +0300

    Example8 updated

--- Example8_IntroMethod/Example8_IntroMethod.cs
+++ Example8_IntroMethod/Example8_IntroMethod.cs
@@ -37,17 +37,19 @@ int Max(int number1, int number2, int number3)
 int a1 = 3;
 int b1 = 24;
 int c1 = 45;
-int a2 = 1;
-int b2 = 14;
+int a2 = 1970;
+int b2 = 14789;
 int c2 = 67;
 int a3 = 387;
 int b3 = 241;
 int c3 = 4;
 
-int max1 = Max(a1, b1, c1);
-int max2 = Max(a2, b2, c2);
-int max3 = Max(a3, b3, c3);
-int max = Max(max1, max2, max3);
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+// int max = Max(max1, max2, max3);
+
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
 
 Console.Write("Max number is ");
 Console.Write(max);
