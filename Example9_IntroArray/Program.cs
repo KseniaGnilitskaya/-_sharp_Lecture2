a67cb0798e5ac29a63f09d641b5b37745cbdd021
Ksenia Gnilitskaya <ksuha_87@mail.ru>
2022-06-16 20:23:30 +0300

    Example9 added

---
+++ Example9_IntroArray/Program.cs
@@ -0,0 +1,15 @@
+﻿
+int Max(int number1, int number2, int number3)
+{
+    int result = number1;
+    if (number2 > result) result = number2;
+    if (number3 > result) result = number3;
+    return result;
+}
+
+int[] = {31,20,43,74,25,66,73,568,119};
+
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+
+Console.Write("Max number is ");
+Console.Write(max);
